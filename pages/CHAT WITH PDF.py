b141a5bd97dcf60457b7005e79e8fe270ee8f0a1
unite-deals <unitedealstest1@gmail.com>
2024-02-28 21:17:57 +0530

    Update CHAT WITH PDF.py

--- pages/CHAT WITH PDF.py
+++ pages/CHAT WITH PDF.py
@@ -29,8 +29,7 @@ def get_text_chunks(text):
 
 
 def get_vector_store(chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(
-        model="models/embedding-001")  # type: ignore
+    GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
     vector_store = FAISS.from_texts(chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
 
